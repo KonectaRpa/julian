1950fa94346585e55abd9908f001bb87b0508d2e
rpabogota <oscar.torres@grupokonecta.com>
2020-01-15 09:22:23 -0500

    Inicial

---
+++ proyectos/CDBK-24/Codigo/Retencion/Core/Selenium/Utilidades.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Selenium
+{
+    public class Utilidades
+    {
+        IWebDriver nav;
+        public void CrearNav()
+        {
+            try
+            {
+                nav = new ChromeDriver();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ proyectos/CDBK-24/Codigo/Retencion/Pantallas/Principal.cs
@@ -0,0 +1,27 @@
+﻿using Core.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pantallas
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Utilidades utilidades = new Utilidades();
+            utilidades.CrearNav();
+        }
+    }
+}
